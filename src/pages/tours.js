fc2734298586732340d6f6ab28925f3de9043402
JunaidKhanEU <junaidkhan1@gmx.com>
2020-02-19 09:18:34 +0000

    pages initiated

---
+++ src/pages/tours.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TourPage extends Component {
+  render () {
+    return (
+      <div>
+        TourPage!!
+      </div>
+    )
+  }
+}
+
+export default TourPage
