90e90a0756015101d90da86dfcb636621fef07e5
JunaidKhanEU <junaidkhan1@gmx.com>
2020-02-19 13:26:34 +0000

    banner and error page done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import SimpleHero from '../components/SimpleHero/SimpleHero'
+import Banner from '../components/Banner/Banner'
+import { Link } from 'gatsby'
 export default () => (
   <Layout>
     Hello from backRoads
+
+    <SimpleHero>
+      <Banner title='continue exploring' info='Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, provident?'>
+        <Link to='/tours' className='btn-white'>explore tours</Link>
+      </Banner>
+    </SimpleHero>
   </Layout>
 )
